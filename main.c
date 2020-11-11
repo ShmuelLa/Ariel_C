75c5df76fd6f1a3adf60ceecf8c506781d57d1e9
Gidon <gidonavziz1@gmail.com>
2020-11-11 15:16:21 +0200

    2

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 #include "myMath.h"
 int main(){
 
-    double ans1,x;
+    double ans1,ans2,ans3,x;
     scanf("pleas: %lf", &x);
     ans1 = sub(add(Exponent(x),Power(x,3)),2);
     printf("The value of f(x) = e^x+x^3-2 at the point %f is : %f",x,ans1);
-
+    ans2 = add(mul(x,3),power(mul(x,2),2));
+    printf("The value of f(x) = e^x+x^3-2 at the point %f is : %f",x,ans2);
+    ans3 = sub(div(power(mul(x,4),3),5),mul(x,2));
+    printf("The value of f(x) = e^x+x^3-2 at the point %f is : %f",x,ans3);
     return 0;
 
 }
